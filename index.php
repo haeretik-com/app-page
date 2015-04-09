eaf45a54db2284baf1bd331ffaf987a17d64f1b8
haeretik-com <haerblog@gmail.com>
2015-04-09 10:55:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,41 @@
 <html>
 <head>
-	<link href="style.css" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 </head>
-
-
 <body>
 
-<div class="head1">ads</div>
-<?php
-	echo "hallo";
-?>
+
+
+<div class="head1">
+	<div class="head-social">
+		<div class="head-socialbuttons">
+			<i class="fa fa-google-plus"></i>
+			<i class="fa fa-youtube"></i>
+			<i class="fa fa-twitter"></i>
+			<i class="fa fa-facebook"></i>
+		</div>
+	</div>
+	<div class="head-nav">
+		<div class="head-navbuttons">
+			<div class="header-logo"><img src="img/header.png" alt="Hearetik Logo"></div>
+			<table class="table-nav">
+				<tr>
+				    <td>Home |</td>
+				    <td>Icon Packs |</td>
+				    <td>CM Themes |</td>
+				    <td>Other |</td>
+				    <td>Contact</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</div>
+
+
+
+
 </body>
-
-
 <footer>
 </footer>
 </html>
